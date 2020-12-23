9d482159c97fab04134ac7022a0b0d93ad9a7ea9
ZeayW <16307110042@fudan.edu.cn>
2020-12-23 15:31:24 +0800

    data_split

--- utils/split_dataset.py
+++ utils/split_dataset.py
@@ -1,30 +1,39 @@
 import os
 import random
-dir = './bottle-dataset'
+dir = 'D:/documents/计算机/计算机视觉/期末pj'
+
+extra_dir = os.path.join(dir,'image_extra')
+extra_list = os.listdir(extra_dir)
+
+
 data_dir = os.path.join(dir,'images')
 
 images_list = os.listdir(data_dir)
+images_list = list(set(images_list).difference(set(extra_list)))
 test_size=int(0.1*len(images_list))
 val_size = int(0.1*len(images_list))
 random.shuffle(images_list)
 test_data = images_list[:test_size]
 val_data = images_list[test_size:test_size+val_size]
 train_data = images_list[test_size+val_size:]
+train_data.extend(extra_list)
+random.shuffle(train_data)
+
 print(len(test_data))
 print(len(val_data))
 print(len(train_data))
-with open('../data/train.txt','w') as f:
+with open('train.txt','w') as f:
     for data in train_data:
-        f.write('../bottle-dataset/images/'+data)
+        f.write('./bottle-dataset/images/'+data)
         f.write('\n')
 
-with open('../data/test.txt','w') as f:
+with open('test.txt','w') as f:
     for data in test_data:
-        f.write('../bottle-dataset/images/'+data)
+        f.write('./bottle-dataset/images/'+data)
         f.write('\n')
-with open('../data/val.txt','w') as f:
+with open('val.txt','w') as f:
     for data in test_data:
-        f.write('../bottle-dataset/images/'+data)
+        f.write('./bottle-dataset/images/'+data)
         f.write('\n')
 # label_dir = os.path.join(dir,'label')
 #
